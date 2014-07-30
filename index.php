d12e05b425dde4034a6e4e7ab6bc32955fd4c147
creative84 <rangeking@hotmail.com>
2014-07-30 11:31:39 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>VdomTest/title>
+<script type="text/javascript" src="/assets/js/jquery.js"></script>
+<script type="text/javascript" src="/assets/js/core.js"></script>
+<script type="text/javascript" src="/assets/js/main.js"></script>
+<link rel="stylesheet" type="text/css" href="/assets/css/core.css">
+</head>
+
+<body>
+
+<div id="home_page"></div>
+
+<div id="main_dialog" title="Main dialog" style="display:none;"></div>
+<div style="display:none"><img src="/img/loading.gif"></div>  
+
+</body>
+</html>
