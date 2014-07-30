c64c77bc7d76f8001fc64b0b6834917b02ff199b
creative84 <rangeking@hotmail.com>
2014-07-30 11:47:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>VdomTest/title>
+<title>VdomTest</title>
 <script type="text/javascript" src="/assets/js/jquery.js"></script>
 <script type="text/javascript" src="/assets/js/core.js"></script>
 <script type="text/javascript" src="/assets/js/main.js"></script>
@@ -10,11 +10,14 @@
 </head>
 
 <body>
+<h3>VdomTest</h3>
+<a href="#home">Home</a> - <a href="#test">Test</a> 
 
 <div id="home_page"></div>
+<div id="test_page"></div>
 
 <div id="main_dialog" title="Main dialog" style="display:none;"></div>
-<div style="display:none"><img src="/img/loading.gif"></div>  
+<div style="display:none"><img src="/assets/img/loading.gif"></div>  
 
 </body>
 </html>
