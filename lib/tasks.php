bf2f5b8d169f7f87d0c0e490e17c7270854ee2aa
creative84 <rangeking@hotmail.com>
2014-08-01 13:33:45 +0200

    Create tasks.php

---
+++ lib/tasks.php
@@ -0,0 +1,28 @@
+PLACE HOLDER 
+
+TAGS
+
+spank
+anal
+gag
+bind
+strok
+tease
+cbt
+milk
+edge
+chastity
+etc
+
+DESIRES
+
+contains tags with grow rate and current value...
+when a value reaches X the dom will look for a task containing that tag
+completing that task lowers the current value of the desire.
+
+TASKS
+
+stuff + tags + desire increment amount
+
+----------------------------------------------------------
+
