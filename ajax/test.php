b8714461500cb0601b3239330902f7564250d69c
creative84 <rangeking@hotmail.com>
2014-08-01 12:37:02 +0200

    Update test.php

--- ajax/test.php
+++ ajax/test.php
@@ -1,5 +1,28 @@
 <?php
+/* AJAX/TEST.PHP
+last update: 01-08-2014
+*/
 
-echo 'this is a test page';
+// LOAD CORE
+require_once '../core.php';
 
+// DATA ARRAY 
+$data_array = array();
+
+/*
+- set db connection
+- use a lib to do stuff... like get contents or so
+	$lib = new lib();
+	$lib->load('getpage');  loads: lib_get
+	$page_text = $lib->getpage->text('home');
+	add $page_text to the $data_array so we can use it in the view	
+*/
+$data_array['page_text'] = 'TEST PAGE TEST 1234';
+
+// LOAD AND GET VIEW 
+$view = new view();
+$html = $view->get('test',$data_array);
+
+// OUTPUT HTML
+echo $html;
 ?>
