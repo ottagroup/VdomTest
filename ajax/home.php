10f2f3f5a641f8c2adea9a8b86d7bed98aef94f3
creative84 <rangeking@hotmail.com>
2014-08-01 12:36:45 +0200

    Update home.php

--- ajax/home.php
+++ ajax/home.php
@@ -1,4 +1,28 @@
 <?php
+/* AJAX/HOME.PHP
+last update: 01-08-2014
+*/
 
-echo 'this is the home page';
+// LOAD CORE
+require_once '../core.php';
+
+// DATA ARRAY 
+$data_array = array();
+
+/*
+- set db connection
+- use a lib to do stuff... like get contents or so
+	$lib = new lib();
+	$lib->load('getpage');  loads: lib_get
+	$page_text = $lib->getpage->text('home');
+	add $page_text to the $data_array so we can use it in the view	
+*/
+$data_array['page_text'] = 'HOME PAGE TEST 1234';
+
+// LOAD AND GET VIEW 
+$view = new view();
+$html = $view->get('home',$data_array);
+
+// OUTPUT HTML
+echo $html;
 ?>
