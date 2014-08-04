8c2031296c86d56aa8275059af568dde1e56b4d8
creative84 <rangeking@hotmail.com>
2014-08-04 18:58:33 +0200

    Create data.php

---
+++ ajax/data.php
@@ -0,0 +1,24 @@
+<?php
+/* AJAX/TEST.PHP
+last update: 04-08-2014
+*/
+
+// LOAD CORE
+require_once '../core.php';
+
+// DATA ARRAY 
+$data_array = array();
+
+// FFDB
+$lib = new lib();
+$lib->load('ffdb'); 
+$result = $lib->ffdb->get_array('testdb.txt');
+$data_array['full'] = $result;
+
+// LOAD AND GET VIEW 
+$view = new view();
+$html = $view->get('data',$data_array);
+
+// OUTPUT HTML
+echo $html;
+?>
