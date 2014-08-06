01d2861e996dbf9c5d1e230d49e7610ef355f5fa
creative84 <rangeking@hotmail.com>
2014-08-06 13:27:19 +0200

    Update data.php

--- view/data.php
+++ view/data.php
@@ -1,2 +1,5 @@
-<p>ffdb data test</p>
+<h1>Flat file database page</h1>
+<p>A simple file to array loader.<br>
+Takes a text file line by line and returns it in a array.</p>
+<p>Bellow a array output from data/testdb.txt</p>
 <pre><?php print_r($data['full']); ?></pre>
