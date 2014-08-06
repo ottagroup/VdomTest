ab8b2abfba7883836bd73d51e8274ec026829f4e
creative84 <rangeking@hotmail.com>
2014-08-06 13:21:16 +0200

    Update home.php

--- ajax/home.php
+++ ajax/home.php
@@ -6,6 +6,8 @@ last update: 01-08-2014
 // LOAD CORE
 require_once '../core.php';
 
+sleep(2.0); // delay to show the loading processs
+
 // DATA ARRAY 
 $data_array = array();
 
@@ -17,7 +19,7 @@ $data_array = array();
 	$page_text = $lib->getpage->text('home');
 	add $page_text to the $data_array so we can use it in the view	
 */
-$data_array['page_text'] = 'HOME PAGE TEST 1234';
+$data_array['page_text'] = '<p>[This text is stored in a data_array and passed to the view file. Where it is then printed.]</p>';
 
 // LOAD AND GET VIEW 
 $view = new view();
